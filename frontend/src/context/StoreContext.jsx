d85f14029e069b559c25552e8fdf3371735f3019
Yashi2305 <singhyashi.800@gmail.com>
2024-11-10 19:18:22 +0530

    Update backend URL to deployed Render URL

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
-  const url = "http://localhost:4000";
+  const url = "https://dinedivine.onrender.com";
   const [food_list, setFoodList] = useState([]);
 
   const addToCart = async (ItemId) => {
